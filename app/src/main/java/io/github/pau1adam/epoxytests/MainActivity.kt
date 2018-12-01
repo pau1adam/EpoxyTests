19fdad404b0853e44f5c77c1e7eff7178a6deee8
Paul Adam <pavlosadam@gmail.com>
2018-12-01 15:30:15 +0200

    Added RecyclerView.

--- app/src/main/java/io/github/pau1adam/epoxytests/MainActivity.kt
+++ app/src/main/java/io/github/pau1adam/epoxytests/MainActivity.kt
@@ -1,12 +1,26 @@
 package io.github.pau1adam.epoxytests
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val recipesController by lazy { RecipesController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.apply {
+            val linearLayoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = recipesController.adapter
+            setHasFixedSize(true)
+            layoutManager = linearLayoutManager
+            addItemDecoration(DividerItemDecoration(this@MainActivity, linearLayoutManager.orientation))
+        }
+        recipesController.requestModelBuild()
     }
 }
